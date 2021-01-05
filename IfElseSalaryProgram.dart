5b930b43f424c3e123d62fdd6f303ee16b8a22d5
Amar Prattipati <75680457+amarprathipati@users.noreply.github.com>
2021-01-06 02:49:32 +0530

    Salary program using if else loops

---
+++ IfElseSalaryProgram.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+void main(){
+  print('Enter your salary:');
+  int totalSalary = int.parse(stdin.readLineSync());
+  print('Enter your expenses:');
+  int allExpenses = int.parse(stdin.readLineSync());100
+
+  if(totalSalary>allExpenses) {
+    print('You have saved ${totalSalary - allExpenses} this month');
+  }
+  else if(allExpenses>totalSalary){
+  print('You have lost ${allExpenses - totalSalary} this month');
+  }
+  else
+  print('Your balance hasn\'t changed');
+}
